6c8659bada151b07619f5a3c1c338010503491f2
unknown <fnumilat@gmail.com>
2020-01-29 21:10:13 -0600

    Routes created

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
-  return <div>NavBar</div>;
+  return (
+    <div>
+      <h1>Nav</h1>
+      <Link to="/">Home</Link>
+      <Link to="/supporter">Support</Link>
+      <Link to="/organization">Organization</Link>
+    </div>
+  );
 }
