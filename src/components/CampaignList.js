e4c68b3be615e56680e8d158ea639de83672e9ef
Ariunbold <ariun1118@gmail.com>
2020-02-03 17:48:51 -0500

    Campaign detail page is started

--- src/components/CampaignList.js
+++ src/components/CampaignList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import CampaignThumbnail from './CampaignThumbnail';
+import Campaign from './Campaign';
 
 export default function CampaignList() {
   return (
     <div>
       {/* we are gonna map through the data and pass into the CampaignCard*/}
       <CampaignThumbnail />
+      <Campaign />
     </div>
   );
 }
