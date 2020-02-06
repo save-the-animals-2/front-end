3d5dfc499e632e759b76e84927b1329621e48c29
unknown <fnumilat@gmail.com>
2020-02-05 22:25:13 -0600

    more work done on org dash two

--- src/components/organizationDashboardTwo.js
+++ src/components/organizationDashboardTwo.js
@@ -60,7 +60,9 @@ const OrganizationDashboardTwoCampaignsList = ({ camp }) => {
       <h4 className="fnu-org-dash-campaign-header">
         Funding Goal: {funding_goal}
       </h4>
-      <button className="fnu-org-dash-campaign-btn">More details</button>
+      <Link key={camp.id} to={`/orgdashcard/${camp.id}`}>
+        <button className="fnu-org-dash-campaign-btn">More details</button>
+      </Link>
     </div>
   );
 };
